099b0401290e3805bc44064fd179b535b8d19ad1
es-sadik <lhcenessadik18@gmail.com>
2022-05-15 14:12:12 +0100

    print facture

--- src/app/modules/formulaire/attestation-travail/attestation-travail.component.ts
+++ src/app/modules/formulaire/attestation-travail/attestation-travail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import jsPDF from 'jspdf';
+
 
 @Component({
   selector: 'app-attestation-travail',
@@ -10,6 +12,22 @@ export class AttestationTravailComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  openPDF(){
+    let DATA: any = document.getElementById('htmlData');
+
+    let pdf = new jsPDF('p', 'pt', 'a2');
+    // No difference
+    pdf.setFontSize(5);
+    // No difference
+   // pdf.setDisplayMode("fullwidth", "continuous","FullScreen");
+    
+    pdf.html(DATA, {
+      callback: (pdf) => {
+        
+        pdf.save("sample.pdf")
+      }
+    }) 
   }
 
 }
